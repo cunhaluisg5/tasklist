89d5d3f53ba8ed11caa4b838fdd88130e5630178
Luís Cipriani <cunhaluisg5@gmail.com>
2023-12-11 20:32:23 -0300

    Configura Eslint e Prettier

--- src/app.js
+++ src/app.js
@@ -2,21 +2,21 @@ import express from 'express';
 
 import routes from './routes.js';
 
-class App{
-  constructor(){
+class App {
+  constructor() {
     this.server = express();
 
     this.middlewares();
     this.routes();
-  };
+  }
 
-  middlewares(){
+  middlewares() {
     this.server.use(express.json());
-  };
+  }
 
-  routes(){
+  routes() {
     this.server.use(routes);
   }
-};
+}
 
 export default new App().server;
