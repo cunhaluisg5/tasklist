c62cded8d2798e40411ab090a9365931bfb480fe
Luís Cipriani <cunhaluisg5@gmail.com>
2023-12-12 19:22:30 -0300

    Carrega models

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,17 @@
+export default {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'admin',
+  database: 'tasklist',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
+
+/*
 module.exports = {
   dialect: "postgres",
   host: "localhost",
@@ -10,3 +24,4 @@ module.exports = {
     underscoredAll: true,
   },
 };
+*/
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/User.js';
+
 const routes = new Router();
 
-routes.get('/teste', (req, res) => {
-  return res.json({ ok: true });
+routes.get('/teste', async (req, res) => {
+  const user = await User.create({
+    name: 'Teste',
+    email: 'teste@teste.com',
+    password_hash: '123456',
+  });
+  return res.json(user);
 });
 
 export default routes;
