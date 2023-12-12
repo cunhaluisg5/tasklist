c8752e8894746b09f5ddc17c2250df1d85db77f6
Luís Cipriani <cunhaluisg5@gmail.com>
2023-12-12 19:39:53 -0300

    Cadastra usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User.js';
+import UserController from './app/controllers/UserController.js';
 
 const routes = new Router();
 
-routes.get('/teste', async (req, res) => {
-  const user = await User.create({
-    name: 'Teste',
-    email: 'teste@teste.com',
-    password_hash: '123456',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
